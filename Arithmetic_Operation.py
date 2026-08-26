0b43a868998830f071f68c351c31f8a95785e347
lgadait-a11y <lgadait@brc.pshs.edu.ph>
2026-08-26 09:12:26 +0800

    Add arithmetic operations for two input numbers
    Implement basic arithmetic operations on two numbers.

---
+++ Arithmetic_Operation.py
@@ -0,0 +1,9 @@
+#Input = enter 2 numbers
+#Output = sum, difference,product,quotienfor of the two numbers
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+
+print(num1 + num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 - num2)
